332332aab600b768156344524f1da1b6ab9ce29b
Giulia <126517423+JuNovais@users.noreply.github.com>
2023-05-10 11:55:31 -0400

    IMC

--- JS/calculos.js
+++ JS/calculos.js
@@ -1,6 +1,20 @@
-function somarValores(){
-    var n1 = parseFloat(numero1.value);
-    var n2 = parseFloat (numero2.value);
-    var soma = n1+n2;
-    resultado.value = soma;
-}
+// document.querySelector('#btnSomar').addEventListener("click", somarValores);/*elemento a chamar*/
+
+// function somarValores(){
+//     var n1 = parseFloat(document.querySelector("#numero1").value);
+//     var n2 = parseFloat(document.querySelector("#numero2").value);
+//     var soma = n1+n2;
+//     document.querySelector("#resultado").value = soma;
+    
+// }
+
+ document.querySelector("#btnSomar").addEventListener("click", function
+ (){
+
+     var n1 = parseFloat(document.querySelector("#peso").value);
+     var n2 = parseFloat(document.querySelector("#altura").value);
+     var IMC = n1/n2*n2;
+    document.querySelector("resultado").value = IMC;
+    
+ });
+
